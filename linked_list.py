3a3e7f90c5e0c4be1e141ade299d20d1294c3612
Trevor <trevirv@gmail.com>
2024-02-01 03:10:16 -0800

    working on tail

--- linked_list.py
+++ linked_list.py
@@ -31,22 +31,41 @@ Phase 2:
 class Node:
   # TODO: Set the `_value` `_next` node instance variables
   def __init__(self, value):
-    pass
+    self._value = value
+    self._next = None
 
 
 # TODO: Implement a Singly Linked List class here
 class LinkedList:
   # TODO: Set the `_head` node, `_tail` node, and list `_length` instance variables
   def __init__(self):
-    pass
+    self._head = None
+    self._tail = None
+    self._length = 0
 
   # TODO: Implement the get_node method here
   def get_node(self, position):
-    pass
+    curr = self._head
+    while curr:
+      if curr._value == position:
+        return curr
+      curr = self._next
+      
 
   # TODO: Implement the add_to_tail method here
   def add_to_tail(self, value):
-    pass
+    new_node = Node(value)
+    if self._head == None:
+      self._head = new_node
+      self._tail = new_node
+      self._length = 1
+      return self._head
+    else:
+      self._tail._next = new_node
+      new_node._tail = self._tail
+      self._length += 1
+      return self._head
+    
 
   # TODO: Implement the add_to_head method here
   def add_to_head(self, value):
@@ -95,12 +114,12 @@ linked_list = LinkedList()
 print(linked_list)                              # <__main__.LinkedList object at ...>
 
 # # 2. Test getting a node by its position
-# print(linked_list.get_node(0))                # None
+print(linked_list.get_node(0))                # None
 
 # # 3. Test adding a node to the list's tail
-# linked_list.add_to_tail('new tail node')
-# print(linked_list.get_node(0))                # <__main__.Node object at ...>
-# print(linked_list.get_node(0)._value)         # `new tail node`
+linked_list.add_to_tail('new tail node')
+print(linked_list.get_node(0))                # <__main__.Node object at ...>
+print(linked_list.get_node(0)._value)         # `new tail node`
 
 # # 4. Test adding a node to list's head
 # linked_list.add_to_head('new head node')
